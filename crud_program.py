062de3c3e816e0311606c72da6595be056bf8696
cheesemaker <cheesemaker@ubuntu.myguest.virtualbox.org>
2024-03-18 16:42:56 -0400

    Added some comments

--- crud_program.py
+++ crud_program.py
@@ -1,22 +1,22 @@
 import psycopg
-import datetime
 
+'''
+Name:       Elie Feghali
+Student Id: 101185489
+'''
+
+# Hard coded specifically for my environment
+# Change as required for yourself
 host     = "localhost"
 port     = "5432"
 database = "School"
 user     = "postgres"
 password = "postgres"
 
-# maybe make it a global so all functions can access thios
+# Global that connect my application to postgres database
 connect = psycopg.connect(f"host={host} port={port} dbname={database} user={user} password={password}")
 
-
-# def connectDatabase():
-
-#     connect = psycopg.connect(f"host={host} port={port} dbname={database} user={user} password={password}")
-    
-#     return connect
-
+# Finds all students in the database and iterates over the student data to display their content
 def getAllStudents():
     students = connect.cursor().execute("SELECT * FROM students").fetchall()
     for student in students:
@@ -24,42 +24,30 @@ def getAllStudents():
             print (col, end="\t")
         print()
 
-
+# Adds a new student to the database given the following parameters
 def addStudent(first_name, last_name, email, enrollment_date):
 
     connect.cursor().execute("""INSERT INTO students (first_name, last_name, email, enrollment_date)
                                 VALUES (%(first_name)s, %(last_name)s, %(email)s, %(enrollment_date)s)""", {'first_name':first_name, 'last_name':last_name, 'email':email, 'enrollment_date':enrollment_date})
 
-
+# Updates a student's email given a student id and a new email
 def updateStudentEmail(student_id, email):
 
     connect.cursor().execute(""" UPDATE students SET email = %(email)s WHERE student_id = %(student_id)s""",
                              {'student_id':student_id, 'email':email})
-    
+
+# Deletes a student from the database given their student id
 def deleteStudent(student_id):
 
     connect.cursor().execute(""" DELETE FROM students WHERE student_id = %(student_id)s""", 
                              {'student_id':student_id})
 
-def printFunc(arr):
-
-    for elem in arr:
-        print(elem)
-
-
-
 def main():
-    # connection = connectDatabase()
-    # getAllStudents()
-    # print()
-    # addStudent('Jim', 'Halpert', 'jimhalpert@dundermifflin.com', '2000-01-01')
-    # updateStudentEmail(7, 'stinkyjimmy@gmail.com')
-    # deleteStudent(4)
-    # getAllStudents()
 
     flag = True
     while(flag):
 
+        # displays all the possible options to test the application
         print("\n \n")
         print("1. getAllStudents()")
         print("2. addStudent()")
@@ -69,6 +57,7 @@ def main():
         option = input("Choose an option from the following: ")
         print("\n \n")
 
+        # Based on the users input, will direct them to execute the requested functionality
         match option:
             case '1':
                 getAllStudents()
@@ -82,7 +71,6 @@ def main():
                 info_list = info.split(" ")
                 updateStudentEmail(int(info_list[0]), info_list[1])
                 print("Successfully updated student!")
-
             case '4':
                 info = int(input("(Student Id): "))
                 deleteStudent(info)
@@ -91,7 +79,9 @@ def main():
                 exit()
 
 
+    # commits the changes to the database, effectively updating the database if needed
     connect.commit()
+    # closes the connection between the application and the database
     connect.close()
 
 
